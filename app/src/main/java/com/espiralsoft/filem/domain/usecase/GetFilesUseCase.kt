f0c2469454fe5e4071300fcd1d370b4bfe1e19ae
kuzan-source <h39krot@gmail.com>
2025-11-09 18:20:23 -0600

    mod: Agrega inyección de dependencias a GetDirectoriesUseCase

--- app/src/main/java/com/espiralsoft/filem/domain/usecase/GetFilesUseCase.kt
+++ app/src/main/java/com/espiralsoft/filem/domain/usecase/GetFilesUseCase.kt
@@ -2,8 +2,9 @@ package com.espiralsoft.filem.domain.usecase
 
 import com.espiralsoft.filem.domain.repository.FileRepository
 import java.nio.file.Path
+import javax.inject.Inject
 
-class GetFilesUseCase(
+class GetFilesUseCase @Inject constructor(
     private val repository: FileRepository
 ) {
     suspend operator fun invoke(path: Path): List<Path> {
