d97a785c5fb05fbbbd33e033cf6413e7a15b1749
kuzan-source <h39krot@gmail.com>
2025-12-05 14:56:23 -0600

    mod: Cambia el tipo de la lista de archivos de Path a FileEntity

--- app/src/main/java/com/espiralsoft/filem/presentation/state/FileListState.kt
+++ app/src/main/java/com/espiralsoft/filem/presentation/state/FileListState.kt
@@ -4,10 +4,11 @@
 
 package com.espiralsoft.filem.presentation.state
 
+import com.espiralsoft.filem.domain.model.FileEntity
 import java.nio.file.Path
 
 data class FileListState(
-    override val files: List<Path> = emptyList(), // Lista de archivos disponibles
+    override val files: List<FileEntity> = emptyList(), // Lista de archivos disponibles
     override val directories: List<Path> = emptyList(), // Lista de directorios disponibles
     override val isLoading: Boolean = false, // Estado de carga
     override val selectedItems: Set<Path> = emptySet(), // Lista de elementos seleccionados
