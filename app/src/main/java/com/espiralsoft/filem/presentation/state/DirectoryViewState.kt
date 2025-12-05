ffa2cd804bf2b266dfdd01d039160a8ffb9c13cd
kuzan-source <h39krot@gmail.com>
2025-12-05 14:44:20 -0600

    mod: Cambio del tipo de lista con los archivos en la interfaz

--- app/src/main/java/com/espiralsoft/filem/presentation/state/DirectoryViewState.kt
+++ app/src/main/java/com/espiralsoft/filem/presentation/state/DirectoryViewState.kt
@@ -1,9 +1,10 @@
 package com.espiralsoft.filem.presentation.state
 
+import com.espiralsoft.filem.domain.model.FileEntity
 import java.nio.file.Path
 
 interface DirectoryViewState {
-    val files: List<Path> // Lista de archivos disponibles
+    val files: List<FileEntity> // Lista de archivos disponibles
     val directories: List<Path> // Lista de directorios disponibles
     val isLoading: Boolean // Estado de carga
     val selectedItems: Set<Path> // Lista de elementos seleccionados
