1a31be913082fda2f1a4f6833922331ddd27c88e
kuzan-source <h39krot@gmail.com>
2025-12-05 14:55:43 -0600

    mod: Cambiado el tipo de retorno de `getFiles` a `List<FileEntity>`

--- app/src/main/java/com/espiralsoft/filem/domain/repository/FileRepository.kt
+++ app/src/main/java/com/espiralsoft/filem/domain/repository/FileRepository.kt
@@ -1,10 +1,11 @@
 package com.espiralsoft.filem.domain.repository
 
+import com.espiralsoft.filem.domain.model.FileEntity
 import java.nio.file.Path
 
 interface FileRepository {
     suspend fun createFile(dirPath: Path, name: String): Boolean // Create
-    suspend fun getFiles(dirPath: Path): List<Path> // Read
+    suspend fun getFiles(dirPath: Path): List<FileEntity> // Read
     suspend fun renameFile(filePath: Path, newName: String): Boolean // Update
     suspend fun moveFile(filePath: Path, targetDir: Path): Boolean // Update
     suspend fun copyFile(filePath: Path, targetDir: Path): Boolean // Update
