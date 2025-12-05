6052cb40187506d86af463ae1f6f29683bee59e2
kuzan-source <h39krot@gmail.com>
2025-12-05 15:07:19 -0600

    feat: Implementa la detección de tipos de archivo

--- app/src/main/java/com/espiralsoft/filem/data/repository/LocalDataRepositoryImpl.kt
+++ app/src/main/java/com/espiralsoft/filem/data/repository/LocalDataRepositoryImpl.kt
@@ -1,8 +1,10 @@
 package com.espiralsoft.filem.data.repository
 
 import com.espiralsoft.filem.data.local.LocalDataSource
+import com.espiralsoft.filem.domain.model.FileEntity
 import com.espiralsoft.filem.domain.repository.DirectoryRepository
 import com.espiralsoft.filem.domain.repository.FileRepository
+import com.espiralsoft.filem.utils.FileType
 import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.withContext
 import java.nio.file.Path
@@ -86,5 +88,25 @@ class LocalDataRepositoryImpl(
     }
     override suspend fun deleteFile(filePath: Path): Boolean {
         TODO("Not yet implemented")
+    }
+
+    /* OTROS */
+
+    private fun detectTypeFromExtension(path: Path): FileType {
+        val extension: String = path.toFile().extension.lowercase()
+        return when (extension) {
+            "jpg", "png", "gif", "bmp", "webp" -> FileType.IMAGE //Imagenes
+            "mp3", "wav", "ogg", "flac", "aac" -> FileType.AUDIO //Audio
+            "mp4", "avi", "mkv", "mov", "3gp", "webm" -> FileType.VIDEO // Video
+            "pdf" -> FileType.PDF // Pdf
+            "doc", "docx", "txt", "rtf", "odt" -> FileType.TEXTO // Documentos
+            "exel" -> FileType.CALCULO // Documentos de calculo
+            "csv", "json", "xml", "bin", "dat" -> FileType.TEXTOPLANO // Archivos de datos
+            "exe" -> FileType.EJECUTABLES // Ejecutables
+            "zip", "rar", "7z", "tar", "gz" -> FileType.COMPRIMIDOS // Comprimidos
+            "html" -> FileType.WEB // Internen
+            "apk" -> FileType.APK //Apks
+            else -> FileType.UNKNOWN // Desconicidos u otros
+        }
     }
 }
