1abba99877d6ab24596c90d0a02eb024fb28f96c
kuzan-source <h39krot@gmail.com>
2025-12-05 15:01:06 -0600

    reft: Refactorizado `FileType` para simplificar y agregar nuevos tipos

--- app/src/main/java/com/espiralsoft/filem/utils/FileType.kt
+++ app/src/main/java/com/espiralsoft/filem/utils/FileType.kt
@@ -1,122 +1,61 @@
 package com.espiralsoft.filem.utils
 
-import android.content.Context
-import android.widget.Toast
 import com.espiralsoft.filem.R
-import java.io.File
 
 /**
  * Definido los distintos tipos de archivos para abrirlos con la app correcta,
  * caracteristicas iconos dependiendo del tipo, y la accion a realizar al hacer click
  */
 enum class FileType(
-    val iconRes: Int, // id del icono
-    val errorIconRes: Int = R.drawable.icon_error, // id del icono de error(igual para todos)
-    val mimeType: String, // tipo de archivo
-    val onClickAction: ((Context, File) -> Unit) // accion a realizar
+    val iconRes: Int,
+    val mimeType: String
 ) {
     IMAGE(
         iconRes = R.drawable.icon_img0,
-        mimeType = "image/*",
-        onClickAction = { context, file ->
-            if (FileOpener.canOpenFile(context, file, "image/*")) {
-                FileOpener.openImage(context, file)
-            } else {
-                Toast.makeText(context, "No hay aplicaciones disponibles para abrir este archivo", Toast.LENGTH_SHORT).show()
-            }
-        }
+        mimeType = "image/*"
     ),
     AUDIO(
         iconRes = R.drawable.icon_audio0,
         mimeType = "audio/*",
-        onClickAction = { context, file ->
-            if (FileOpener.canOpenFile(context, file, "audio/*")) {
-                FileOpener.openAudio(context, file)
-            } else {
-                Toast.makeText(context, "No hay aplicaciones disponibles para abrir este archivo", Toast.LENGTH_SHORT).show()
-            }
-        }
     ),
     VIDEO(
         iconRes = R.drawable.icon_video0,
         mimeType = "video/*",
-        onClickAction = { context, file ->
-            if (FileOpener.canOpenFile(context, file, "video/*")) {
-                FileOpener.openVideo(context, file)
-            } else {
-                Toast.makeText(context, "No hay aplicaciones disponibles para abrir este archivo", Toast.LENGTH_SHORT).show()
-            }
-        }
-    ),
-    DOCUMENT(
-        iconRes = R.drawable.icon_document0,
-        mimeType = "text/*",
-        onClickAction = { context, file ->
-            if (FileOpener.canOpenFile(context, file, "text/*")) {
-                FileOpener.openText(context, file)
-            } else {
-                Toast.makeText(context, "No hay aplicaciones disponibles para abrir este archivo", Toast.LENGTH_SHORT).show()
-            }
-        }
     ),
     PDF(
         iconRes = R.drawable.icon_pdf0,
         mimeType = "application/pdf",
-        onClickAction = { context, file ->
-            if (FileOpener.canOpenFile(context, file, "application/pdf")) {
-                FileOpener.openPdf(context, file)
-            } else {
-                Toast.makeText(context, "No hay aplicaciones disponibles para abrir este archivo", Toast.LENGTH_SHORT).show()
-            }
-        }
     ),
-    ARCHIVE(
+    TEXTO(
+        iconRes = R.drawable.icon_document0,
+        mimeType = "text/*",
+    ),
+    CALCULO(
+        iconRes = R.drawable.icon_sheet0,
+        mimeType = "TODO",
+    ),
+    TEXTOPLANO(
+        iconRes = R.drawable.icon_plaintext0,
+        "text/*"
+    ),
+    EJECUTABLES(
+        iconRes = R.drawable.icon_exec0,
+        mimeType = "TODO",
+    ),
+    COMPRIMIDOS(
         iconRes = R.drawable.icon_archive0,
-        mimeType = "application/zip",
-        onClickAction = { context, file ->
-            if (FileOpener.canOpenFile(context, file, "application/zip")) {
-                FileOpener.openCompressed(context, file)
-            } else {
-                Toast.makeText(context, "No hay aplicaciones disponibles para abrir este archivo", Toast.LENGTH_SHORT).show()
-            }
-        }
+        mimeType = "TODO",
     ),
-    DATAFILE(
-        iconRes = R.drawable.icon_datafile0,
-        mimeType = "application/octet-stream",
-        onClickAction = { context, file ->
-            if (FileOpener.canOpenFile(context, file, "application/octet-stream")) {
-                FileOpener.openText(context, file)
-            } else {
-                Toast.makeText(context, "No hay aplicaciones disponibles para abrir este archivo", Toast.LENGTH_SHORT).show()
-            }
-        }
+    WEB(
+        iconRes = R.drawable.icon_web0,
+        mimeType = "TODO",
+    ),
+    APK(
+        iconRes = R.drawable.icon_apk0,
+        mimeType = "TODO",
     ),
     UNKNOWN(
         iconRes = R.drawable.icon_unkdown,
         mimeType = "",
-        onClickAction = { context, _ ->
-            Toast.makeText(context, "No se puede abrir este tipo de archivo", Toast.LENGTH_SHORT).show()
-        }
-    );
-
-    // Obtener el tipo de archivo a partir del nombre del archivo
-    // Solo se identifican los listados abajo, los demas se consideran desconocidos
-    // NOTA: Asegurarse de identifcar los mas comunes asi como sus correspondientes categorias
-    companion object {
-        fun fromFile(file: File): FileType {
-            val extension = file.extension.lowercase()
-            return when (extension) {
-                "jpg", "jpeg", "png", "gif", "bmp", "webp" -> IMAGE //Extenciones de imagenes
-                "mp3", "wav", "ogg", "flac", "aac" -> AUDIO //Extenciones de audio
-                "mp4", "avi", "mkv", "mov", "3gp", "webm" -> VIDEO // Extenciones de video
-                "doc", "docx", "txt", "rtf", "odt" -> DOCUMENT // Extenciones de documentos
-                "pdf" -> PDF // Extenciones de pdf
-                "zip", "rar", "7z", "tar", "gz" -> ARCHIVE // Extenciones de archivos comprimidos
-                "csv", "json", "xml", "bin", "dat" -> DATAFILE // Extenciones de archivos de datos
-                else -> UNKNOWN // Desconicidos
-            }
-        }
-    }
-
+    )
 }
